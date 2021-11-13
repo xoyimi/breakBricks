9c42479134d3b33335baabc3b31955f0f63f2182
xoyimi <xoyimi@foxmail.com>
2021-11-14 02:47:00 +0800

    dist

--- src/sense/sense.ts
+++
@@ -1,9 +0,0 @@
-import type Game from '../js/Game'
-export default abstract class Sense {
-  game: Game
-  constructor(game: Game) {
-    this.game = game
-  }
-  abstract update(): void
-  abstract draw(): void
-}
--- src/sense/start/StartSense.ts
+++
@@ -1,9 +0,0 @@
-import Sense from '../sense'
-
-export class StartSense extends Sense {
-  name: 'startSense'
-  constructor() {
-    super()
-    this.name = 'startSense'
-  }
-}
